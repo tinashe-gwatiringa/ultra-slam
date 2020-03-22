0fc27d66b615a29ea144545268e177d5df4141d1
Darryn Jordan <darryn.jordan@dronesar.com>
2020-03-22 13:48:21 +0200

    clarified function intent

--- driver.py
+++ driver.py
@@ -2,11 +2,11 @@ import subprocess
 from multiprocessing import Pool
 
 
-def run(path):
+def run_script(path):
     subprocess.call(['python3', path])
 
 
 if __name__ == '__main__':
     scripts = ['web_server/app.py', 'slam/app.py']
     pool = Pool(len(scripts))
-    pool.map(run, scripts)
+    pool.map(run_script, scripts)
