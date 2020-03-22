cdeb3cc6a987aca0b075f7973bde66f416618474
Darryn Jordan <darryn.jordan@dronesar.com>
2020-03-22 15:22:41 +0200

    added git reset and pull

--- driver.py
+++ driver.py
@@ -7,6 +7,9 @@ def run_script(path):
 
 
 if __name__ == '__main__':
+    subprocess.call(['git', 'reset', '--hard'])
+    subprocess.call(['git', 'pull'])
+
     scripts = ['web_server/app.py', 'slam/app.py']
     pool = Pool(len(scripts))
     pool.map(run_script, scripts)
