e9748826c3dd8fb39e63593306987e454acf1031
Darryn Jordan <darryn.jordan@dronesar.com>
2020-03-22 15:26:06 +0200

    added requirements update

--- driver.py
+++ driver.py
@@ -7,9 +7,14 @@ def run_script(path):
 
 
 if __name__ == '__main__':
+    # pull from repo
     subprocess.call(['git', 'reset', '--hard'])
     subprocess.call(['git', 'pull'])
 
+    # install any missing requirements
+    subprocess.call(['pip3', 'install', '-r', 'web_server/requirements.txt'])
+
+    # run scripts as separate processes
     scripts = ['web_server/app.py', 'slam/app.py']
     pool = Pool(len(scripts))
     pool.map(run_script, scripts)
